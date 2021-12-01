2ac435cb572a345d8e6ce894eac4be990f08e7ca
kip0215 <67665667+kippumchagn215@users.noreply.github.com>
2021-12-01 05:48:51 -0600

    organize with functions, add comment

--- Network Project/Client/src/Client.java
+++ Network Project/Client/src/Client.java
@@ -6,57 +6,56 @@ class Client {
 
     public static void main(String argv[]) throws Exception 
     { 
-        String instruction; 
-        String modifiedSentence; 
-        
+        String instruction;// string to store instruction that was sent from the server
+        String expResult;  // string to store result of math expresssion returned from server
         BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
+          new BufferedReader(new InputStreamReader(System.in)); // used to read users input
         
-//        String Hostname = InetAddress.getLocalHost().toString();
-//        System.out.println(Hostname);
         
-        Socket clientSocket = new Socket("127.1.0.0", 6000);
+        Socket clientSocket = new Socket("127.1.0.0", 6000); // create socket to connect with the server
         
-    	DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
+        // reaching here means that socket was successfully accepted by the server
+        
+    	DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());  // used to send data to the server
     	        
-    	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+    	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // used to receive data from the server
     	
-        String UserInput = new String();
+        String UserInput = new String(); // string to store users input
         	
-        int NumofInput = 0;
+        int NumofInput = 0; // used to count number of input user has entered
         
-        instruction = inFromServer.readLine();
-        System.out.println(instruction);
+        instruction = inFromServer.readLine(); // store the instruction about how to use the calculator from the server
+        System.out.println(instruction); // print out the instruction here
         
         while(true) {
         	
         	System.out.println("Please Enter Basic Math Expressions Or Press x to quit");
         	
-        	UserInput = inFromUser.readLine();
+        	UserInput = inFromUser.readLine(); // get users input
         	
-        	if(UserInput.toLowerCase().equals("x")) {
+        	if(UserInput.toLowerCase().equals("x")) { // if user wants to exit
         		
-        		if(NumofInput>=3) {break;}
-        		else {
-        			System.out.println("Please Enter At Least 3 Math Expression");continue;
+        		if(NumofInput>=3) {break;} // first check if user has entered at least 3 expressions, if true break out of infinite loop
+        		else { 
+        			System.out.println("Please Enter At Least 3 Math Expression");continue; // if user has not entered 3 expression,print instruction
         		}
         		
         	}else {
-        		NumofInput++;
+        		NumofInput++; // as user entered expression add one
         	}
         	
-	        outToServer.writeBytes(UserInput + '\n'); 
+	        outToServer.writeBytes(UserInput + '\n'); // send math expression to server
 	        
-	        modifiedSentence = inFromServer.readLine(); 
+	       expResult = inFromServer.readLine();  // read the result from the server
 	
-	        System.out.println("FROM SERVER: " + modifiedSentence);
+	        System.out.println("FROM SERVER: " +expResult); 
                     
         }
         	 
-	        clientSocket.close();
+	        clientSocket.close(); // close the socket
 	        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z"); // to format the date in a way that is easy to read for users
         	String disconInfo = MessageFormat.format("Disconnected from server at {0}",formatter.format(new Date(System.currentTimeMillis())));
-        	System.out.println(disconInfo);
+        	System.out.println(disconInfo); // print when client disconnected from the server
 	        
           
     } 
@@ -65,6 +64,7 @@ class Client {
 
         
 // Question to ask
-//1. does the name that client sends to server, can it be ipaddress or does it have to be a name?
-//2. is running a client code multiple times a valid solution to create multiple clients?
-//3. what is message format? for protocol
+// name of host not being returned in some cases
+// what does request mean, does it actually have to send some message to the server, or cleintSocket.close() is this considered a request?
+// is my way of handling in order a valid way
+// can port number and ip address be hard coded?
--- Network Project/Server/src/Server.java
+++ Network Project/Server/src/Server.java
@@ -4,7 +4,6 @@ import java.text.*;
 import java.util.Date;
 import java.io.File;
 import java.io.FileWriter;
-import java.math.*;
 
 class Server {
 	public static int reqnum = 0; // assign a reqnum number to each request that was sent from client, ex- first request will have reqnum number 1
@@ -17,20 +16,20 @@ class Server {
       	while(!welcomeSocket.isClosed()) { // while the socket created is open
       		 
 
-      		 Socket connectionSocket = welcomeSocket.accept(); // open the socket for to receive connection request client
+      		 Socket connectionSocket = welcomeSocket.accept(); // open the socket and receive connection request from client
       		 
       		 new ServerThread(connectionSocket).start(); // create a new thread for each client
 
              
       	}
       	
-      welcomeSocket.close();
+      welcomeSocket.close(); // close the socket, but it might never called since the server should always be running to handle client request
 	} 
 
 }
 
 
- class ServerThread extends Thread {
+ class ServerThread extends Thread {	
 	 
     public Socket connectionSocket;
 
@@ -44,16 +43,18 @@ class Server {
     public void run() {
     	String clientSentence; 
         String result;
+        int CurReqNum = 0;
         
     	long start = System.nanoTime(); // when the client connection is accepted and the thread is created for the client, start the timer
+    	
 		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z"); // to format the date in a way that is easy to read for users
 	    Date date = new Date(System.currentTimeMillis()); // assign current date and time to date variable
-	    String ipAddress = connectionSocket.getRemoteSocketAddress().toString(); // get the ipaddress of a client using getremotesocketaddress()
 	    
-	    String connectedMsg = MessageFormat.format("IP address of connected client is {0} and connected at {1} ", ipAddress,formatter.format(date));
-	    System.out.println(connectedMsg);
-	    writeToFile(connectedMsg);
- 	     
+	    String ipAddress = connectionSocket.getRemoteSocketAddress().toString(); // get the ipaddress of a client using getremotesocketaddress()
+	    String Hostname = connectionSocket.getInetAddress().getCanonicalHostName().toString();
+	    
+	    printConnectedMsg(Hostname,ipAddress,date,formatter);// print info of the client when it is connected with the server
+	    
  		BufferedReader inFromClient; // used to read input from user
  		DataOutputStream outToClient; // used to send outputs to the user
 		
@@ -62,7 +63,7 @@ class Server {
 			outToClient= new DataOutputStream(connectionSocket.getOutputStream());
 			
 			String statusMsg = MessageFormat.format("Connected with Client with IpAddress {0} Successfully, please enter a math expression with one operator(+,-,*,/,^) and two operands, ex - 44/4, 4^9",ipAddress);
-			outToClient.writeBytes(statusMsg + '\n');
+			outToClient.writeBytes(statusMsg + '\n'); // function - send instruction
 			
 			 while(!connectionSocket.isClosed()) { // while the connection with client is still connected 
 	           	 
@@ -74,27 +75,23 @@ class Server {
 	           		 long finish = System.nanoTime(); // as soon as the connection is disconnected, measure the time
 	           		 long timeElapsed = (finish - start)/ 1_000_000_000; // subtract finish time with start time to calculate how long the client has been connected to the server
 	           		 
-	           		String disconMsg = MessageFormat.format("Client with Ipaddress {0} has disconnected",ipAddress);
-	           		String disconInfo = MessageFormat.format("Client with was connected for {0} and got disconnected at {1}",timeElapsed,formatter.format(new Date(System.currentTimeMillis())));
-	        	    System.out.println(disconMsg);
-	        	    System.out.println(disconInfo);
-	        	    writeToFile(disconMsg);
-	        	    writeToFile(disconInfo);
+	           		 printDisconMsg(ipAddress,timeElapsed,formatter);// print info of the client when it is disconnects from the server
 	           		 
 	           	 }else { // if client is still connected, respond
+	           		 
 	           		 	Server.reqnum++; // every time the server receives a request from client is gives reqnum number to each request
-	           		 	result = calculate(clientSentence) != null ? 
-	           		 			 calculate(clientSentence)+ '\n':
+	           		 	
+	           		 	CurReqNum = Server.reqnum; // assign static reqnum to local CurReqNum to maintain the value,because the static req num will change everytime its called by different thread
+	           		 	
+	           		 	String validExpCheck = calculate(clientSentence);
+	           		 	result = validExpCheck!= null ? 
+	           		 			 validExpCheck+ '\n':
 	           		 			 "Error" + '\n';
-	           		 	String reqMsg = MessageFormat.format("request number: {0} Client''s requested expression = {1} result = {2}",Server.reqnum,clientSentence,result);
-	           		 	System.out.println(reqMsg);
-	           		 	writeToFile(reqMsg);
-	           		 	// while loop under first check if the previous request has already been handled by comparing handled number with current reqnum number -1
-	           		 	// if the condition is false, it means that the result of previous request has not yet sent, so it will loop infinitely 
-	           		 	// until the previous thread sends back the result and increment the value of handled
-	           		 	while(Server.handled!=Server.reqnum-1&&Server.reqnum!=1) {
-	           		 		System.out.println("The previous request has not been handled yet"); // printout the reason the result of current request is not being sent
-	           		 	}
+	           		 	
+	           		 	printReqMsg(clientSentence,Hostname,ipAddress,result);// when req received, display who client is and what expression was received
+	           		 	
+	           		 	waitForTurn(CurReqNum); // function to send the result back in a order the server received the req
+	           		 	
 	                    outToClient.writeBytes(result); // send back the result to the client
 	                    Server.handled++; // increment the value of handled to show that the current reqnum has been successfully handled.
 	           	 }
@@ -105,32 +102,67 @@ class Server {
 		} 
     }
     
+    // function to print info of the client when it is connected with the server on console and logfile
+    public void printConnectedMsg(String Hostname,String ipAddress,Date date,SimpleDateFormat formatter) {
+    	String connectedMsg = MessageFormat.format("Client Connected! Host {0} with IPaddress {1} connected at {2} ",Hostname, ipAddress,formatter.format(date));
+	    System.out.println(connectedMsg);
+	    writeToFile(connectedMsg);// function - print connected clients info
+    }
+    // function to  print info of the client when it is disconnects from the server on console and logfile 
+    public void printDisconMsg(String ipAddress,long timeElapsed,SimpleDateFormat formatter) {
+    	String disconMsg = MessageFormat.format("Client with Ipaddress {0} has disconnected",ipAddress);
+   		String disconInfo = MessageFormat.format("Client with was connected for {0} and got disconnected at {1}",timeElapsed,formatter.format(new Date(System.currentTimeMillis())));
+	    System.out.println(disconMsg);
+	    System.out.println(disconInfo);
+	    writeToFile(disconMsg);
+	    writeToFile(disconInfo); // function print and log Clients Info
+    }
+    // function to  print when req received, display who client is and what expression was received on console and logfile
+    public void printReqMsg(String clientSentence,String Hostname,String ipAddress,String result) {
+    	String reqMsg = MessageFormat.format("request number: {0}, Received expression = {1}, From Hostname: {2} with IpAddress:{3}, result = {4}",Server.reqnum,clientSentence,Hostname,ipAddress,result);
+		System.out.println(reqMsg);
+		writeToFile(reqMsg);// function print client req info
+    }
+    
+    // function to send the result back in a order the server received the req
+ 	/* while loop under first check if the previous request has already been handled by comparing handled number with current reqnum number -1
+ 	if the condition is false, it means that the result of previous request has not yet sent, so it will loop infinitely 
+ 	until the previous thread sends back the result and increment the value of handled*/
+    
+    public void waitForTurn(int curThreadReqNum) {
+    	while(Server.handled!=curThreadReqNum-1) {
+		 	System.out.println("The previous request has not been handled yet"); // printout the reason the result of current request is not being sent
+		}
+    }
+    
 	// function to calculate expression received from client, capable of caculating expression with two operands with one operator, ex 4*22, 3+7 
     public String calculate(String exp) {		
     	
-        String parsedExp = exp.replaceAll(" ","");
-        String [] parsedArray;
+        String parsedExp = exp.replaceAll(" ",""); // remove all spaces
+        String [] parsedArray; // array to store tokens
         
         Double res = 0.0;
         try {
-            if(parsedExp.contains("-")) {
-            	parsedArray = parsedExp.split("-");
+            if(parsedExp.contains("-")) { // if operator is -
+            	parsedArray = parsedExp.split("-"); // split using operator, so only the operands are left
             	res = Double.parseDouble(parsedArray[0]) - Double.parseDouble(parsedArray[1]);
-            }else if(parsedExp.contains("+")) {
+            }else if(parsedExp.contains("+")) {// if operator is +
             	parsedArray = parsedExp.split("\\+");
             	res =  Double.parseDouble(parsedArray[0]) + Double.parseDouble(parsedArray[1]);
-            }else if(parsedExp.contains("/")) {
+            }else if(parsedExp.contains("/")) {// if operator is /
             	parsedArray = parsedExp.split("/");
             	res =  Double.parseDouble(parsedArray[0]) / Double.parseDouble(parsedArray[1]);
-            }else if(parsedExp.contains("*")) {
+            }else if(parsedExp.contains("*")) {// if operator is *
             	parsedArray = parsedExp.split("\\*");
             	res =  Double.parseDouble(parsedArray[0]) * Double.parseDouble(parsedArray[1]);
-            }else if(parsedExp.contains("^")) {
+            }else if(parsedExp.contains("^")) {// if operator is ^
             	parsedArray = parsedExp.split("\\^");
             	res =  Math.pow(Double.parseDouble(parsedArray[0]),Double.parseDouble(parsedArray[1]));
             }
-            String result = MessageFormat.format("{0, number, #.##}",res);
+            String result = MessageFormat.format("{0, number, #.##}",res); // limit the decimal two 2nd place
+            
             return result;
+            
         }catch(NullPointerException e) {
         	return null;
         }    
